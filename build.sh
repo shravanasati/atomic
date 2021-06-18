9c13d660ff98e9c86862d6a5ab3222a35b5b9a02
Shravan Asati <dev.shravan@protonmail.com>
2021-06-18 14:00:03 +0530

    👽️ did some exception handling

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_name="./bin/hydra-$GOOS-$GOARCH"
+    output_name="./bin/bench-$GOOS-$GOARCH"
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi
--- src/bench.go
+++ src/bench.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/thatisuday/commando"
 	"os"
 	"os/exec"
 	"strconv"
 	"strings"
 	"text/template"
 	"time"
+
+	"github.com/thatisuday/commando"
 )
 
 const (
@@ -16,7 +17,10 @@ const (
 	VERSION = "0.1.0"
 )
 
+// Result struct which is shown at the end as benchmarking summary and is written to a file.
 type Result struct {
+	Started    string
+	Ended      string
 	Command    string
 	Iterations int
 	Average    time.Duration
@@ -31,21 +35,25 @@ func main() {
 		SetVersion(VERSION).
 		SetDescription("bench is a simple CLI tool to make benchmarking easy.")
 
+	// * root command
 	commando.
 		Register(nil).
 		AddArgument("command", "The command to run for benchmarking.", "").
-		AddArgument("iterations", "The number of times to iterate for benchmarking", "10").
+		AddArgument("iterations", "The number of iterations.", "10").
 		SetAction(func(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
+			// * initialising some variables
 			executable := args["command"].Value
 			command := strings.Fields(executable)
 			x, e := strconv.Atoi(args["iterations"].Value)
 			if e != nil {
-				fmt.Println("Wrong input for iterations.")
+				fmt.Println("Invalid input for iterations value.")
 				return
 			}
 			var sum time.Duration
 
+			started := time.Now().Format("02-01-2006 15:04:05")
 			for i := 1; i <= x; i++ {
+				fmt.Printf("***********\nRunning iteration %v\n***********\n", i)
 				cmd := exec.Command(command[0], command[1:]...)
 				_, e := cmd.StdoutPipe()
 				if e != nil {
@@ -55,24 +63,41 @@ func main() {
 				cmd.Stdout = os.Stdout
 				cmd.Stderr = os.Stderr
 				init := time.Now()
-				cmd.Start()
-				cmd.Wait()
+				if e := cmd.Start(); e != nil {
+					fmt.Println("The command couldnt be started!")
+					fmt.Println(e)
+					return
+				}
+				if e := cmd.Wait(); e != nil {
+					fmt.Println("The command failed to execute!")
+					fmt.Println(e)
+					return
+				}
 				sum += time.Since(init)
 			}
+			ended := time.Now().Format("02-01-2006 15:04:05")
+
+			// * result text
 			text := `
 Benchmarking Summary
 --------------------
 
+Started: {{ .Started }}
+Ended: {{ .Ended }}
 Executed Command: {{ .Command }}
 Total iterations: {{ .Iterations }}
 Average time taken: {{ .Average }}
 `
+			// * intialising the template struct
 			result := Result{
-				executable,
-				x,
-				((sum) / time.Duration(x)),
+				Started: started,
+				Ended: ended,
+				Command: executable,
+				Iterations: x,
+				Average: sum / time.Duration(x),
 			}
 
+			// * parsing the template
 			tmpl, err := template.New("result").Parse(text)
 			if err != nil {
 				panic(err)
